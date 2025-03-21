d4dff8e08d6f0cfb67fc09b7467948655979ec05
Srivatsan-Srinivasan-L <srivatsan042@gmail.com>
2025-03-21 23:51:17 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css'
 
-import './App.css'
-import Home from './pages/Home'
+
+import Header from './pages/Header';
+import Banner from './pages/Banner';
+import Footer from './pages/Footer';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
 
 function App() {
-
-
   return (
-    <>
-   
-   <BrowserRouter>
-  <div>
-<Home/>
-    <Routes>
-      
-     
-
-
-    </Routes>
-    
-  </div>
-  
-  
-  </BrowserRouter>
-
-      
-   
-    </>
-  )
+        
+    <Router>
+            <Header/>
+            <Banner/>
+            <Contact/>
+            <Services/>
+            <Footer/>
+      <Routes>
+       
+        <Route path="/about" element={<Banner />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
